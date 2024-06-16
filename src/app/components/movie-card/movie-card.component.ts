b97bc79b3ff51cf83074168a4c7c3abfecdc81c4
Kyrylo Shevchenko <kir971@gmail.com>
2024-06-17 00:22:07 +0300

    Change styles of card from custom to material UI

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,13 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IMovieCard } from '../../models/IMovieCard';
 import { DurationPipe } from '../../pipes/duration/duration.pipe';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [ DurationPipe ],
+  imports: [
+    DurationPipe,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+  ],
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss'
+  styleUrl: './movie-card.component.scss',
 })
 export class MovieCardComponent {
   @Input() movie!: IMovieCard;
