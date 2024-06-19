437349672a7b520e65fa91ef72ddf7c85ee1c57e
Kyrylo Shevchenko <kir971@gmail.com>
2024-06-19 23:07:45 +0300

    Create pages

---
+++ src/app/pages/top-rated-page/top-rated-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-rated-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-rated-page.component.html',
+  styleUrl: './top-rated-page.component.scss'
+})
+export class TopRatedPageComponent {
+
+}
