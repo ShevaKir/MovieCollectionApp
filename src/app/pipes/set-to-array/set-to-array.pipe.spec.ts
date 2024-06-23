e09ff1eef09cb004914cfcf76a778cd6fa05b043
Kyrylo Shevchenko <kir971@gmail.com>
2024-06-24 02:17:08 +0300

    Remove pipe

--- src/app/pipes/set-to-array/set-to-array.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SetToArrayPipe } from './set-to-array.pipe';
-
-describe('SetToArrayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SetToArrayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
