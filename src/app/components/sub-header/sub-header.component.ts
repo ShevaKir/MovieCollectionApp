c8ff4bd1e1a93580a4090f94fa7296a7b719ca65
Kyrylo Shevchenko <kir971@gmail.com>
2024-06-27 22:13:44 +0300

    Add active tab

--- src/app/components/sub-header/sub-header.component.ts
+++ src/app/components/sub-header/sub-header.component.ts
@@ -3,21 +3,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { SelectMovieList } from '../../enums/SelectMovieList';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sub-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, MatButtonModule],
+  imports: [CommonModule, MatToolbarModule, RouterLink, MatButtonModule],
   templateUrl: './sub-header.component.html',
   styleUrl: './sub-header.component.scss',
 })
 export class SubHeaderComponent {
   @Output() selectedTab = new EventEmitter<SelectMovieList>();
   SelectMovieList = SelectMovieList;
+  currentTab: SelectMovieList = SelectMovieList.Empty;
 
   constructor() {}
 
   selectTab(tab: SelectMovieList) {
+    this.currentTab = tab;
     this.selectedTab.emit(tab);
   }
 }
