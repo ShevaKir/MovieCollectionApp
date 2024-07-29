4c04b41702bc3c2f9dfcccbc89aaca1b2f21c291
Kyrylo Shevchenko <kir971@gmail.com>
2024-07-29 22:04:47 +0300

    Tests of watch later and favorite

--- src/app/pages/favorite/favorite.component.spec.ts
+++ src/app/pages/favorite/favorite.component.spec.ts
@@ -1,17 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FavoriteComponent } from './favorite.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { MemoizedSelector } from '@ngrx/store';
+import { selectFavoriteMovies } from '../../store/selectors';
+import { IMovie } from '../../models/movie.model';
+import { removeFromFavorite } from '../../store/actions';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FavoriteComponent', () => {
   let component: FavoriteComponent;
   let fixture: ComponentFixture<FavoriteComponent>;
+  let store: MockStore;
+  let mockSelectFavoriteMovies: MemoizedSelector<any, IMovie[]>;
+
+  const mockFavoriteMovies: IMovie[] = [
+    {
+      adult: false,
+      backdrop_path: '/path/to/backdrop.jpg',
+      genre_ids: [28, 12, 16],
+      id: 1,
+      original_language: 'en',
+      original_title: 'Original Title',
+      overview: 'This is a test overview.',
+      popularity: 8.5,
+      poster_path: '/path/to/poster.jpg',
+      release_date: '2024-07-27',
+      title: 'Test Movie',
+      video: false,
+      vote_average: 7.5,
+      vote_count: 1000
+    }
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FavoriteComponent]
-    })
-    .compileComponents();
-    
+      imports: [NoopAnimationsModule, FavoriteComponent],
+      providers: [
+        provideMockStore()
+      ]
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+    mockSelectFavoriteMovies = store.overrideSelector(selectFavoriteMovies, mockFavoriteMovies);
+
     fixture = TestBed.createComponent(FavoriteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +50,21 @@ describe('FavoriteComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render favorite movies', () => {
+    fixture.detectChanges(); // Ensure changes are detected
+
+    const compiled = fixture.nativeElement;
+    const movieTitle = compiled.querySelector('.list__item__title');
+    
+    expect(movieTitle).toBeTruthy(); // Ensure the element exists
+    expect(movieTitle.textContent).toContain('Test Movie');
+  });
+
+  it('should dispatch removeFromFavorite action when removeMovieFromFavouriteList is called', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.removeMovieFromFavouriteList(1);
+    expect(dispatchSpy).toHaveBeenCalledWith(removeFromFavorite({ id: 1 }));
   });
 });
--- src/app/pages/watch-later/watch-later.component.spec.ts
+++ src/app/pages/watch-later/watch-later.component.spec.ts
@@ -1,17 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WatchLaterComponent } from './watch-later.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { MemoizedSelector } from '@ngrx/store';
+import { IMovie } from '../../models/movie.model';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { selectWatchLaterMovies } from '../../store/selectors';
+import { removeFromWatchLater } from '../../store/actions';
 
 describe('WatchLaterComponent', () => {
   let component: WatchLaterComponent;
   let fixture: ComponentFixture<WatchLaterComponent>;
+  let store: MockStore;
+  let mockSelectWatchLaterMovies: MemoizedSelector<any, IMovie[]>;
+
+  const mockWatchLaterMovies: IMovie[] = [
+    {
+      adult: false,
+      backdrop_path: '/path/to/backdrop.jpg',
+      genre_ids: [28, 12, 16],
+      id: 1,
+      original_language: 'en',
+      original_title: 'Original Title',
+      overview: 'This is a test overview.',
+      popularity: 8.5,
+      poster_path: '/path/to/poster.jpg',
+      release_date: '2024-07-27',
+      title: 'Test Movie',
+      video: false,
+      vote_average: 7.5,
+      vote_count: 1000,
+    },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [WatchLaterComponent]
-    })
-    .compileComponents();
-    
+      imports: [WatchLaterComponent, NoopAnimationsModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+    mockSelectWatchLaterMovies = store.overrideSelector(
+      selectWatchLaterMovies,
+      mockWatchLaterMovies
+    );
+
     fixture = TestBed.createComponent(WatchLaterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +53,18 @@ describe('WatchLaterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render watch later movies', () => {
+    const compiled = fixture.nativeElement;
+    const movieTitle = compiled.querySelector('.list__item__title');
+
+    expect(movieTitle).toBeTruthy();
+    expect(movieTitle.textContent).toEqual('Test Movie');
+  });
+
+  it('should dispatch removeFromWatchLater action when removeMovieFromWatchLaterList is called', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.removeMovieFromWatchLaterList(1);
+    expect(dispatchSpy).toHaveBeenCalledWith(removeFromWatchLater({id: 1}))
+  })
 });
