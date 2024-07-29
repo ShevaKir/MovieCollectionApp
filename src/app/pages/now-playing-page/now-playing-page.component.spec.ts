41ae29c55e6003cd63484d1434f1a90a417a8f7c
Kyrylo Shevchenko <kir971@gmail.com>
2024-07-30 02:10:45 +0300

    Fixed created tests

--- src/app/pages/now-playing-page/now-playing-page.component.spec.ts
+++ src/app/pages/now-playing-page/now-playing-page.component.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NowPlayingPageComponent } from './now-playing-page.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 describe('NowPlayingPageComponent', () => {
   let component: NowPlayingPageComponent;
   let fixture: ComponentFixture<NowPlayingPageComponent>;
+  let httpMock: HttpTestingController;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NowPlayingPageComponent]
+      imports: [NowPlayingPageComponent, HttpClientTestingModule],
+      providers: [
+        provideMockStore()
+      ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(NowPlayingPageComponent);
+    httpMock = TestBed.inject(HttpTestingController);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/pages/upcoming-page/upcoming-page.component.spec.ts
+++ src/app/pages/upcoming-page/upcoming-page.component.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpcomingPageComponent } from './upcoming-page.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 describe('UpcomingPageComponent', () => {
   let component: UpcomingPageComponent;
   let fixture: ComponentFixture<UpcomingPageComponent>;
+  let httpMock: HttpTestingController;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UpcomingPageComponent]
+      imports: [UpcomingPageComponent, HttpClientTestingModule],
+      providers: [
+        provideMockStore()
+      ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(UpcomingPageComponent);
+    httpMock = TestBed.inject(HttpTestingController);
+    store = TestBed.inject(MockStore);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
